828c37306f022e64adf6a4560b3af6a95bf4d195
cek0621 <ekc0621@gmail.com>
2021-12-16 15:06:53 +0900

    노래 예약기능 완성

--- 노래방.c
+++ 노래방.c
@@ -10,6 +10,7 @@ typedef struct queue{
 //	ELEMENT * buf;
 	ELEMENT * num;
 	CHARACTER * name;
+	QUEUE * link;
 	int front;
 	int rear;
 	int size;
@@ -23,28 +24,44 @@ void destroyQueue( QUEUE * q );
 void printAll( QUEUE *q );
 int main() {
 	QUEUE *q;
-	ELEMENT data;
-	q = createQueue( 8 );
+//	ELEMENT data;
+	q = createQueue( 2 );
 	Song* song = (Song *)malloc(sizeof(song));
-//	scanf("%d",&(a->Snum));
-//	printf("%d",a->Snum);
+ 
 	int num; 
 	while(1){
-		printf("----------------------------------------------------------\n");
-		printf("1.노래예약   2.노래 시작  3.예약 현황   4.예약한 노래 검색\n");
-		printf("----------------------------------------------------------\n");
+		printf("--------------------------------------------------------------------\n");
+		printf("1.노래예약   2.노래 시작  3.예약 현황   4.예약한 노래 검색  5.끝내기\n");
+		printf("--------------------------------------------------------------------\n");
 		scanf("%d",&num);
 		switch(num){
 			case 1:
+				printf("노래 번호를 입력하세요 :");
 				scanf("%d",&(song->Snum));
+				printf("\n노래 제목을 입력하세요 :");
 				scanf("%s",(song->Sname));
+//				gets(song->Sname);
 				enQueue(q,song->Snum,song->Sname);
+				if(isEmpty(q)>=1){
+					printf("%s이(가) 예약이 되었습니다.\n",song->Sname);
+				}
+				
 			 	break;
-			default:
+			case 2: 
 				return 0;
+				break;
+			case 3:
+				printAll(q);
+				break;
+			case 4:
+				return 0;
+				break;
+			case 5:
+				return 0;
+				break;
 		}
 	}
-	
+//	printAll(q);
 
 	return 0;
 }
@@ -61,21 +78,21 @@ QUEUE * createQueue( int n ){
 int isFull( QUEUE *q ){
 	 if(q->rear >= q->size-1){
 	  	return 1;	
-	 }// 큐가 가득 차 있다면 rear 은 어떤 상태일까?
+	 }	
 	 else{
 		  return -1;
 	 }
 }
 void enQueue( QUEUE *q, ELEMENT nu, CHARACTER na ){
 	 if(isFull(q)==1){
-	 		printf("queue is full\n");	
-	 }// 가득 차 있다면 "queue is full"이라고 출력
+	 		printf("예약이 가득찼습니다.\n");	
+	 }
 	 else{
 	 		q -> rear++;
 //		 	q->buf[q->rear] = data;
 		 	q ->name[q->rear] = na;
 			q->num [q->rear] =  nu;
-	 }// 아니면 rear에 data 삽입
+	 }
 }
 int isEmpty( QUEUE *q ){
 	if(q->rear == q->front){
@@ -89,22 +106,21 @@ ELEMENT deQueue( QUEUE *q ){
 	if(isEmpty(q)==1){
 		printf("Queue is empty\n");
 		return 0;
-	}	// 비어 있다면 "Queue is empty" 출력하고 0 리턴
+	}
   else{
 		q->front++;
 		return q->num[q->front];
-	}	// 아니면 front위치의 데이터 리턴
+	}
 }
 void printAll( QUEUE *q ){
 	int i;
-	for(i = q->front + 1;i<=q->rear;i++){
-		printf("%d ",q->name[i]);
-		printf("%d ",q->num[i]);
+	for(i = q->front + 1;i<= q->rear;i++){
+		printf("%s",q->name[i]);
 	}
 }
 
 void destroyQueue( QUEUE * q ){
-	free(q->name);// buf 동적 메모리 해제
+	free(q->name);
 	free(q->num);
-	free(q);// q 동적 메모리 해제
+	free(q);
 }
